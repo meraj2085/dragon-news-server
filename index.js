7ac50c1bf41ec9bf49fbe07392b8917a541030b6
meraj2085 <samirmeraj60@gmail.com>
2022-10-20 11:22:37 +0600

    adding all categories

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.get("/news-categories", (req, res) => {
   res.send(categories);
 });
 
+app.get('/news', (req, res)=>{
+  res.send(news)
+})
+
 app.get("/news/:id", (req, res) => {
   const id = req.params.id;
   const selectedNews = news.find((n) => n._id === id);
